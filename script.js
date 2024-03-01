50732579e9adf14462df6cacc2782f4dfc6ebc9c
Hayat-07 <hayat1607207@gmail.com>
2024-02-29 22:05:45 -0800

    "2nd commit"

---
+++ script.js
@@ -0,0 +1,49 @@
+const leftMainDiv= document.querySelector(".container");
+
+const  setLeftMainDiv=(x)=>{
+   
+    fetch(`./pages/left-${x}.html`)
+    .then(res=>{
+        if(res.ok){
+            console.log("res.next() has been called",res);
+            return res.text();
+        }
+    })
+    .then(myHtml=>{
+        leftMainDiv.innerHTML=myHtml;
+        console.log("INNERHTML has been called", myHtml);
+    })
+    .catch(err=>console.log(err));
+   
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

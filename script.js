7848c8654bb6cf6fe7d09ce74f8e99aebd201297
Hayat-07 <hayat1607207@gmail.com>
2024-03-03 18:35:37 -0800

    "6th commit"

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-const leftMainDiv= document.querySelector(".container");
+const container= document.querySelector(".container");
 
-const  setLeftMainDiv=(x)=>{
+const  setRightMainDiv=(x)=>{
    
-    fetch(`./pages/left-${x}.html`)
-    .then(res=>{
-        if(res.ok){
-            // console.log("res.next() has been called",res);
-            return res.text();
-        }
-    })
-    .then(myHtml=>{
-        leftMainDiv.innerHTML=myHtml;
-        // console.log("INNERHTML has been called", myHtml);
-    })
-    .catch(err=>console.log(err));
+   
+        fetch(`./pages/left-${x}.html`)
+        .then(res=>{
+            if(res.ok){
+                // console.log("res.next() has been called",res);
+                return res.text();
+            }
+        })
+        .then(myHtml=>{
+            container.innerHTML=myHtml;
+            // console.log("INNERHTML has been called", myHtml);
+        })
+        .catch(err=>console.log(err));
+  
    
 };
+
+
+
+
+
 
 
 
